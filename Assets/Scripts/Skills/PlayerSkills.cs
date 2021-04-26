0765354099915d422d731c1993a6b805d535dd52
Dan <kolisnychenko.dan2019@gmail.com>
2021-04-26 19:07:44 +0200

    Aded blank skills

---
+++ Assets/Scripts/Skills/PlayerSkills.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Skills
+{
+    public class PlayerSkills : MonoBehaviour
+    {
+        public UniversalInventorySystem.Inventory skills;
+            
+        private void Start()
+        {
+            skills.Initialize();
+        }
+    }
+}
+
---
+++ Assets/Scripts/Skills/Skill.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniversalInventorySystem;
+
+namespace RPG.Skills
+{
+    [ 
+        CreateAssetMenu(fileName = "Skill", menuName = "Skill"), 
+        System.Serializable
+    ]
+    public class Skill : UniversalInventorySystem.Item
+    {
+        [SerializeField] public float coolDown = 0f;       
+    }
+}
